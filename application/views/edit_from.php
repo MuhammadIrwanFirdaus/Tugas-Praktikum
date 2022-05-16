69edf1bdc71d44cdad4c5ec37b969e3e7a0d315b
MuhammadIrwanFirdaus <irwanfirdaus107@gmail.com>
2022-05-16 10:04:16 +0800

    commit pertama

--- application/views/edit_from.php
+++ application/views/edit_from.php
@@ -1,21 +1,21 @@
 
 <main>
-<div class="container-fluid">
-<h1 class="mt-4"></h1>
-<ol class="breadcrumb mb-4">
-<li class="breadcrumb-item"><a href="<?php echo site_url('admin/user') ?>">User</a></li>
-<li class="breadcrumb-item active"><?php echo $title ?></li>
-</ol>
-<div class="card mb-4">
-<div class="card-body">
-<form action="<?php echo site_url('admin/surat_masuk/edit') ?>" method="post" enctype="multipart/form-data" >
-<div class="mb-3">
-<label >NO SURAT MASUK <code>*</code></label>
-<input class="form-control" type="hidden" name="id" value="<?=$surat->id;?>" required />
-<input class="form-control" type="text" name="no_surat" value="<?=$surat->no_surat;?>" placeholder="NO SURAT MASUK" required />
-</div>
-<div class="mb-3">
-<label >PERIHAL SURAT <code>*</code></label>
+    <div class="container-fluid">
+        <h1 class="mt-4"></h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="<?php echo site_url('admin/user') ?>">User</a></li>
+                <li class="breadcrumb-item active"><?php echo $title ?></li>
+            </ol>
+            <div class="card mb-4">
+                <div class="card-body">
+                    <form action="<?php echo site_url('admin/surat_masuk/edit') ?>" method="post" enctype="multipart/form-data" >
+                        <div class="mb-3">
+                            <label >NO SURAT MASUK <code>*</code></label>
+                                <input class="form-control" type="hidden" name="id" value="<?=$surat->id;?>" required />
+                                <input class="form-control" type="text" name="no_surat" value="<?=$surat->no_surat;?>" placeholder="NO SURAT MASUK" required />
+                        </div>
+                        <div class="mb-3">
+                        <label >PERIHAL SURAT <code>*</code></label>
 <input class="form-control" type="text" name="perihal" value="<?=$surat->perihal;?>" placeholder="PERIHAL SURAT" required />
 </div>
 <div class="mb-3">
